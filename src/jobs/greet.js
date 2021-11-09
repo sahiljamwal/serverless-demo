b4620a4ca1aa844bf9304c38299c1e35193a5857
sahil-jamwal <sahil.jamwal78625@gmail.com>
2021-11-10 01:25:53 +0530

    update : 1-intro | added event based lambda trigger and setup for serverless-offline

--- src/jobs/greet.js
+++ src/jobs/greet.js
@@ -1,6 +1,9 @@
 module.exports.sayHello = async (evt, ctx) => {
+  console.log("event:::", evt);
+  console.log("context:::", ctx);
+
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hey There !!" }),
+    body: JSON.stringify({ evtName: "greetInvoker", message: evt }),
   };
 };
